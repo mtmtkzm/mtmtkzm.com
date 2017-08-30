78671c18693c98586ee06309e80fc3e5b910b63f
mtmtkzm <hello@mtmtkzm.com>
2017-08-30 16:42:01 +0900

    Update github select necessary-data

--- src/js/get-github.js
+++ src/js/get-github.js
@@ -9,11 +9,27 @@ export default class getLigblog {
   request() {
     axios.get(this.API_PATH)
       .then(response => {
-        console.log(response);
+        this.selectNecessaryData(response);
       })
       .catch(error => {
         console.log('error', error);
       });
+  }
+
+  selectNecessaryData (response) {
+    let necessaryData = [];
+    let pushEventArray = response.data.filter(item => {
+      if (item.type === 'PushEvent') { return true }
+    }).forEach( item => {
+      necessaryData.push({
+        date: new Date(item.created_at),
+        title: item.repo.name,
+        desc: item.payload.commits[item.payload.commits.length - 1].message,
+        link: `https://github.com/${item.repo.name}/commit/${item.payload.commits[item.payload.commits.length - 1].sha}`,
+      });
+    });
+ 
+    console.log(necessaryData);
   }
 
 }
