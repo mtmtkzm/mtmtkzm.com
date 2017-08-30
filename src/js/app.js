b6ee8042889f988642e07e0c5d770e1ce1cf4557
mtmtkzm <hello@mtmtkzm.com>
2017-08-30 18:44:18 +0900

    Update sort myActivities in descending order

--- src/js/app.js
+++ src/js/app.js
@@ -14,29 +14,35 @@ let flickr = new getFlickr();
 let github = new getGithub();
 
 Promise
-	.all([
-		new Promise( (resolve, reject) => {
-			codepen.request(resolve, reject);
-		}),
-		new Promise( (resolve, reject) => {
-			qiita.request(resolve, reject);
-		}),
-		new Promise( (resolve, reject) => {
-			ligblog.request(resolve, reject);
-		}),
-		new Promise( (resolve, reject) => {
-			flickr.request(resolve, reject);
-		}),
-		new Promise( (resolve, reject) => {
-			github.request(resolve, reject);
-		})
-	])
-	.then( () => {
-		let myActivities = codepen.returnData().concat(
-			qiita.returnData(),
-			ligblog.returnData(),
-			flickr.returnData(),
-			github.returnData()
-		);
-		console.log(myActivities);
-	});
+    .all([
+        new Promise( (resolve, reject) => {
+            codepen.request(resolve, reject);
+        }),
+        new Promise( (resolve, reject) => {
+            qiita.request(resolve, reject);
+        }),
+        new Promise( (resolve, reject) => {
+            ligblog.request(resolve, reject);
+        }),
+        new Promise( (resolve, reject) => {
+            flickr.request(resolve, reject);
+        }),
+        new Promise( (resolve, reject) => {
+            github.request(resolve, reject);
+        })
+    ])
+    .then( () => {
+        let myActivities = codepen.returnData().concat(
+            qiita.returnData(),
+            ligblog.returnData(),
+            flickr.returnData(),
+            github.returnData()
+        );
+
+        myActivities.sort(function(a, b){
+            if(a.date > b.date) return -1;
+            if(a.date < b.date) return 1;
+            return 0;
+        });
+        console.log(myActivities);
+    });
