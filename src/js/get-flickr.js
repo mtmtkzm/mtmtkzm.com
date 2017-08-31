b800f6e5a2ed1a670f91b6721b7135f581be0fad
mtmtkzm <hello@mtmtkzm.com>
2017-08-31 16:32:02 +0900

    Add remove unnecessary code from flickr request

--- src/js/get-flickr.js
+++ src/js/get-flickr.js
@@ -8,34 +8,9 @@ export default function getFlickr () {
       'method': 'flickr.people.getPhotos',
       'api_key': 'ca30f5ed9e9016cdd5a47455053319da',
       'user_id': '144899726@N03',
-      'format': 'json',
-      'nojsoncallback': 1,
-    }
-  })
-    .then( response => {
-      let allPromises = [];
-
-      response.data.photos.photo.forEach( item => {
-        allPromises.push(requestPhotoInfo(item.id));          
-      });
-
-      return Promise
-        .all(allPromises)
-        .then( value => {
-          return Array.prototype.concat.apply([], value);
-        });
-    })
-    .catch( error => {
-      console.log('error', error);
-    });
-}
-
-function requestPhotoInfo (photoId) {
-  return axios.get(API_PATH, {
-    params: {
-      'method':'flickr.photos.getInfo',
-      'api_key': 'ca30f5ed9e9016cdd5a47455053319da',
-      'photo_id': photoId,
+      'min_upload_date': '',
+      'extras': 'last_update,description',
+      'per_page': 10,
       'format': 'json',
       'nojsoncallback': 1,
     }
@@ -50,13 +25,14 @@ function requestPhotoInfo (photoId) {
 
 function selectNecessaryData (response) {
   let necessaryData = [];
-  let photo = response.data.photo;
-  necessaryData.push({
-    type: 'flickr',
-    date: Number(photo.dates.lastupdate + '000'), // Flickrは ミリ秒 ではなく 秒 を返す
-    title: photo.title._content,
-    desc: photo.description._content,
-    url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_n.jpg`,
+  response.data.photos.photo.forEach(item => {
+    necessaryData.push({
+      type: 'flickr',
+      date: Number(item.lastupdate + '000'), // Flickrは ミリ秒 ではなく 秒 を返す
+      title: item.title,
+      desc: item.description._content,
+      url: `https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}_n.jpg`,
+    });
   });
 
   return necessaryData;
