92b841a23ffaa482c6ffda60d4001968745906f8
mtmtkzm <hello@mtmtkzm.com>
2018-05-22 23:53:14 +0900

    とりあえず無理やりトップページを移植

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,27 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        portfolio desu
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div id="app">
+    <div :class="['loading', {'is-loaded': isLoaded}]"></div>
+    <div class="wave"></div>
+    <div class="container">
+      <main class="main">
+
+        <!--<activity></activity>-->
+      </main>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  import {mapGetters} from 'vuex';
+  // import activity from 'components/activity';
 
-export default {
-  components: {
-    AppLogo
+  export default {
+    name: 'App',
+    components: {
+      // activity,
+    },
+    computed: mapGetters({
+      isLoaded: 'isLoaded'
+    })
   }
-}
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
