01833cc24d6315ad1e1039157af31e6875c4672a
mtmtkzm <hello@mtmtkzm.com>
2019-01-01 00:29:42 +0900

    Update エラーページがいろいろエラーだった

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,48 +3,73 @@
 
     <div class="introduction">
       <template v-if="error.statusCode === 404">
-        <h2>NOT FOUND</h2>
-        <p>This page is no longer available. Please look for the page from the following list. Sorry for inconvenience.</p>
-        <p>
-          アクセスされたページはすでに削除されたか、ご利用いただけません。下記リストよりお目当のページをお探しください。また、サイト内のリンク切れを発見した場合、<a href="//twitter.com/mtmtkzm" target="_blank">Twitter <icon :id="`twitter`"/></a>、もしくは<a href="mailto:mtmtkzm00@gmail.com">メール <icon :id="`mail`"/></a>にて管理者までご一報いただけますと幸いです。早急に対応させていただきます。
-        </p>
-      </template>
-      <template v-else>
-        <h2>ERROR</h2>
+        <h2>404</h2>
+        <ArticleBody>
+          <p>This page is no longer available. Please look for the page from the following list. Sorry for inconvenience.</p>
+          <p>
+            アクセスされたページは削除されたか、現在ご利用いただけません。<br>
+            下記リストよりお目当のページをお探しください。また、サイト内のリンク切れを発見した場合、
+            <a href="//twitter.com/mtmtkzm" target="_blank"><AppIcon name="twitter"/> Twitter</a>、もしくは
+            <a href="mailto:mtmtkzm00@gmail.com"><AppIcon name="mail"/> メール</a>にてご一報いただけますと幸いです。
+            早急に対応させていただきます。多分いっぱいあると思います...。
+          </p>
+
+          <h3>ページ</h3>
+          <ul>
+            <li>
+              <nuxt-link to="/">TOP PAGE</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/about">ABOUT ME</nuxt-link>
+            </li>
+          </ul>
+
+          <h3>外部リンク</h3>
+          <ul>
+            <li>
+              <a
+                href="//github.com/mtmtkzm"
+                target="_blank"
+              >
+                GitHub</a>
+            </li>
+            <li>
+              <a
+                href="//twitter.com/mtmtkzm"
+                target="_blank"
+              >
+                Twitter</a>
+            </li>
+            <li>
+              <a
+                href="//www.wantedly.com/users/17812476"
+                target="_blank"
+              >
+                Wantedly</a>
+            </li>
+          </ul>
+        </ArticleBody>
       </template>
     </div>
-
-    <ul>
-      <li>
-        <nuxt-link to="/">TOP PAGE</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/">ABOUT ME</nuxt-link>
-      </li>
-    </ul>
-
-    <ul>
-      <li>
-        <a href="//github.com/mtmtkzm">GitHub</a>
-      </li>
-      <li>
-        <a href="//twitter.com/mtmtkzm">Twitter</a>
-      </li>
-      <li>
-        <a href="//www.wantedly.com/users/17812476">Wantedly</a>
-      </li>
-    </ul>
   </div>
 </template>
 <script>
+  import ArticleBody from '~/components/common/ArticleBody'
+
   export default {
     props: {
       error: {},
     },
+    components: {
+      ArticleBody
+    }
   }
 </script>
 
 <style scoped lang="scss">
+  .introduction {
+    padding: 0 0 45px;
+  }
   .error {
     max-width: var(--max-width);
     margin: 0 auto;
@@ -56,31 +81,4 @@
     font-weight: bold;
     padding: 48px 0 10px;
   }
-
-  p {
-    & + p {
-      margin-top: 24px;
-    }
-
-    a {
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      height: 1em;
-      border-bottom: 1px solid var(--secondary-color);
-
-      .icon {
-        width: 14px;
-        margin: -2px 4px 0;
-      }
-    }
-
-  }
-
-  ul {
-    margin-top: 24px;
-    margin-left: 18px;
-    list-style-type: circle;
-  }
-
 </style>
