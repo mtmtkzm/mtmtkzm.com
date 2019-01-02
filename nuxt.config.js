b950bfe6896ecbf48a3fb1c5fcbd6def199f6fb9
mtmtkzm <hello@mtmtkzm.com>
2019-01-02 22:02:47 +0900

    Add とりあえず雑に追加した

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,43 @@
+const fs = require('fs');
 const path = require('path');
-const spriteSvg = require('nuxt-sprite-svg-inline');
+
+function customizeAppTemplate () {
+  fs.readFile(path.join(__dirname, 'assets/svg/icons.svg'), 'utf8', function (err, data) {
+    if (err) throw err;
+
+    fs.open(path.join(__dirname, '.nuxt/views/app.template.html'), 'w', function (err, fd) {
+      if (err) throw err;
+
+      const resultStr =
+        '<!--' +
+        '\n __    __ _______ __    __ _______ _   ________ __    __' +
+        '\n|  \\  /  |__   __|  \\  /  |__   __| | / /___  /|  \\  /  |' +
+        '\n| \\ \\/ / |  | |  | \\ \\/ / |  | |  | |/ /   / / | \\ \\/ / |' +
+        '\n| |\\  /| |  | |  | |\\  /| |  | |  |  _ \\  / /__| |\\  /| |' +
+        '\n|_| \\/ |_|  |_|  |_| \\/ |_|  |_|  |_| \\_\\/_____|_| \\/ |_|' +
+        '\n-->' +
+        '<!DOCTYPE html>\n' +
+        '<html {{ HTML_ATTRS }}>\n' +
+        '  <head>\n' +
+        '    {{ HEAD }}\n' +
+        '  </head>\n' +
+        '  <body {{ BODY_ATTRS }}>\n' +
+        data +
+        '    {{ APP }}\n' +
+        '  </body>\n' +
+        '</html>';
+      const buffer = new Buffer(resultStr);
+
+      fs.write(fd, buffer, 0, buffer.length, null, function (err) {
+        if (err) throw err;
+        fs.close(fd, function () {
+          // done!
+        })
+      })
+    })
+
+  })
+}
 
 const general = {
   title: 'MTMTKZM - Front-End Developer ライダーのポートフォリオサイト',
@@ -72,10 +110,7 @@ module.exports = {
       //   })
       // }
       if (isServer) {
-        spriteSvg.run(
-          path.join(__dirname, 'assets/svg/icons.svg'),
-          path.join(__dirname, '.nuxt/views/app.template.html')
-        )
+        customizeAppTemplate();
       }
     },
   },
