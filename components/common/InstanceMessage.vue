a8f068545f81026286d06c35a845ad0113952709
mtmtkzm <hello@mtmtkzm.com>
2019-02-24 12:38:28 +0900

    Add ０文字の時は送信できないようにする

--- components/common/InstanceMessage.vue
+++ components/common/InstanceMessage.vue
@@ -7,14 +7,19 @@
     data-netlify="true"
     method="post"
   >
-    <input type="hidden" name="form-name" value="instance-message" />
+    <input type="hidden" name="form-name" value="instance-message"/>
     <textarea
+      v-model="message"
       name="message"
       placeholder="Say Hello!"
     >
     </textarea>
     <div class="instance-message-footer">
-      <button class="instance-message-submit" type="submit">
+      <button
+        class="instance-message-submit"
+        type="submit"
+        v-bind:disabled="(message.length < 1)"
+      >
         <AppIcon name="send"/>
         <span>Submit</span>
       </button>
@@ -22,6 +27,16 @@
     </div>
   </form>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: ''
+      }
+    },
+  };
+</script>
 
 <style scoped lang="postcss">
   .instance-message {
@@ -67,6 +82,7 @@
     border: 1px solid #fff;
     outline: none;
     cursor: pointer;
+    transition: opacity var(--base-transition);
 
     &:focus {
       .app-icon {
@@ -81,6 +97,11 @@
       width: 14px;
       height: 14px;
       transition: all var(--base-transition);
+    }
+
+    &[disabled] {
+      opacity: .5;
+      cursor: not-allowed;
     }
   }
 </style>
