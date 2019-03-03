ee6801b6d98b232dea250cddcbda1cab73839e25
mtmtkzm <hello@mtmtkzm.com>
2019-03-04 00:42:40 +0900

    Update styles

--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,21 @@
     <section class="hello">
       <h2 @click="toggleShowWorld">
         <span>Hello,</span>
-        <transition
-          name="hello-world"
-        >
-          <span
-            class="hello-world"
-            v-show="showWorld"
-          >
+        <transition name="hello-world">
+          <span class="hello-world" v-show="showWorld">
             <AppIcon name="globe"/>
           </span>
         </transition>
       </h2>
       <ArticleBody>
-        <p>
-          初めまして、ライダーです。<br>
-          株式会社LIGでフロントエンドエンジニアとして、受託でWebサイトやWebサービスの開発をしています。
+        <p>初めまして、ライダーです。
+          <br>株式会社LIGでフロントエンドエンジニアとして、受託でWebサイトやWebサービスの開発をしています。
         </p>
 
         <p>仕事（会社）では、かっこいいものより便利なものを作ることに携わりたいと思っています。</p>
 
         <p class="more-about">
-          <nuxt-link to="/about">
-            More profile
+          <nuxt-link to="/about">More profile
             <AppIcon name="arrow-right"/>
           </nuxt-link>
         </p>
@@ -33,16 +26,10 @@
 
     <section class="articles">
       <h2>Articles</h2>
-      <template v-if="isLoadingArticles">
-        Loading Articles ....
-      </template>
+      <template v-if="isLoadingArticles">Loading Articles ....</template>
       <template v-else>
         <ul class="article-list">
-          <li
-            v-for="article in articles"
-            :key="article.url"
-            class="article-item"
-          >
+          <li v-for="article in articles" :key="article.url" class="article-item">
             <a :href="article.url" target="_blank">
               <article>
                 <div class="article-item-header">
@@ -106,12 +93,7 @@
       <h2>Works</h2>
 
       <div class="works-list">
-        <a
-          href="//maner.gallery"
-          target="_blank"
-          rel="noopener"
-          class="work work-l work-maner"
-        ></a>
+        <a href="//maner.gallery" target="_blank" rel="noopener" class="work work-l work-maner"></a>
         <a
           href="//liginc.co.jp/author/kazuma"
           target="_blank"
@@ -130,39 +112,39 @@
 </template>
 
 <script>
-  import ArticleBody from '~/components/common/ArticleBody';
+import ArticleBody from "~/components/common/ArticleBody";
 
-  export default {
-    components: {
-      ArticleBody
+export default {
+  components: {
+    ArticleBody
+  },
+  data() {
+    return {
+      showWorld: false
+    };
+  },
+  computed: {
+    articles: function() {
+      return this.$store.getters.recentArticles;
     },
-    data () {
-      return {
-        showWorld: false,
-      }
-    },
-    computed: {
-      articles: function () {
-        return this.$store.getters.recentArticles;
-      },
-      isLoadingArticles: function () {
-        return this.$store.state.articles.isLoading;
-      }
-    },
-    mounted: function () {
-      this.$store.dispatch('getArticles');
-    },
-    methods: {
-      toggleShowWorld: function () {
-        this.showWorld = !this.showWorld;
-      }
-    },
+    isLoadingArticles: function() {
+      return this.$store.state.articles.isLoading;
+    }
+  },
+  mounted: function() {
+    this.$store.dispatch("getArticles");
+  },
+  methods: {
+    toggleShowWorld: function() {
+      this.showWorld = !this.showWorld;
+    }
   }
+};
 </script>
 
 <style scoped lang="postcss">
-  .hello {
-    padding: 0 24px;
+.hello {
+  padding: 0 24px;
 
   h2 {
     font-size: 56px;
@@ -172,26 +154,26 @@
     display: flex;
     align-items: center;
 
-  .hello-world {
-    display: flex;
-    align-items: center;
-    margin-left: 12px;
+    .hello-world {
+      display: flex;
+      align-items: center;
+      margin-left: 12px;
 
-  .app-icon {
-    width: 48px;
-    height: 48px;
-  }
-  }
+      .app-icon {
+        width: 48px;
+        height: 48px;
+      }
+    }
 
-  .hello-world-enter-active,
-  .hello-world-leave-active {
-    transition: all var(--base-transition);
-  }
-  .hello-world-enter,
-  .hello-world-leave-to {
-    opacity: 0;
-    transform: scale(.95);
-  }
+    .hello-world-enter-active,
+    .hello-world-leave-active {
+      transition: all var(--base-transition);
+    }
+    .hello-world-enter,
+    .hello-world-leave-to {
+      opacity: 0;
+      transform: scale(0.95);
+    }
   }
 
   .more-about {
@@ -200,40 +182,49 @@
     justify-content: flex-end;
     margin-top: 12px;
 
-  a {
-    display: flex;
-    align-items: center;
-  }
-
-  .app-icon {
-    margin-left: 12px;
-  }
-  }
-  }
-
-  .articles {
-    padding: 0 24px;
-
-    h2 {
-      font-size: 56px;
-      font-weight: bold;
-      padding: 36px 0 0;
+    a {
+      display: flex;
+      align-items: center;
     }
 
-    .article-item {
-      border-top: 1px solid var(--color-gray);
+    .app-icon {
+      margin-left: 12px;
+    }
+  }
+}
 
-      a {
+.articles {
+  padding: 0 24px;
+
+  h2 {
+    font-size: 56px;
+    font-weight: bold;
+    padding: 36px 0 0;
+  }
+
+  .article-item {
+    &:not(:first-of-type) {
+      &::before {
+        content: '';
         display: block;
         width: 100%;
-        height: 100%;
-        padding: 24px;
+        height: 1px;
+        background-color: var(--color-gray);
+        transform: scaleY(0.25);
       }
     }
-  }
 
-  .career {
-    padding: 0 24px;
+    a {
+      display: block;
+      width: 100%;
+      height: 100%;
+      padding: 16px 0;
+    }
+  }
+}
+
+.career {
+  padding: 0 24px;
 
   h2 {
     font-size: 56px;
@@ -247,26 +238,26 @@
     display: flex;
     align-items: center;
 
-  &::before,
-  &::after {
-     content: '';
-     display: block;
-     height: 1px;
-     background-color: var(--color-secondary);
-     transform: scaleY(0.25);
-     flex: 1;
-     transition: background-color var(--base-transition);
-   }
+    &::before,
+    &::after {
+      content: "";
+      display: block;
+      height: 1px;
+      background-color: var(--color-secondary);
+      transform: scaleY(0.25);
+      flex: 1;
+      transition: background-color var(--base-transition);
+    }
 
-  &::before {
-     flex: 1;
-     margin-right: 10px;
-   }
+    &::before {
+      flex: 1;
+      margin-right: 10px;
+    }
 
-  &::after {
-     flex: 32;
-     margin-left: 10px;
-   }
+    &::after {
+      flex: 32;
+      margin-left: 10px;
+    }
   }
 
   ul {
@@ -276,10 +267,9 @@
   li {
     font-size: 13px;
   }
-  }
+}
 
-  .works {
-
+.works {
   h2 {
     font-size: 56px;
     font-weight: bold;
@@ -298,31 +288,29 @@
     height: 50vw;
     max-height: calc(var(--max-width) / 2);
 
-  &-l {
-     width: 100%;
-   }
+    &-l {
+      width: 100%;
+    }
 
-  &-m {
-     width: 50%;
-   }
+    &-m {
+      width: 50%;
+    }
 
-  &-s {
-     width: 33.3333%;
-   }
-
+    &-s {
+      width: 33.3333%;
+    }
   }
 
   .work-maner {
-    background: url('~assets/images/works/maner.png') center center/cover;
+    background: url("~assets/images/works/maner.png") center center/cover;
   }
 
   .work-lig-projects {
-    background: url('~assets/images/works/lig-projects.png') center center/cover;
+    background: url("~assets/images/works/lig-projects.png") center center/cover;
   }
 
   .work-lig-articles {
-    background: url('~assets/images/works/lig-articles.png') center center/cover;
+    background: url("~assets/images/works/lig-articles.png") center center/cover;
   }
-
-  }
+}
 </style>
