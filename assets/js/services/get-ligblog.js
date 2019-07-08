d825a2fdd5ce49086a44327102c78b90d2dcb256
mtmtkzm <hello@mtmtkzm.com>
2019-07-08 20:38:18 +0900

    Add assets を移動

---
+++ assets/js/services/get-ligblog.js
@@ -0,0 +1,29 @@
+import * as u from '../utils';
+import http from '../utils/http';
+const API_PATH = 'https://liginc.co.jp/wp-json/wp/v2/posts';
+
+export default function () {
+  return http.get(API_PATH, {
+    params: {
+      'author': '396',
+      'per_page': '10'
+    }
+  })
+    .then(response => {
+      return formatData(response);
+    })
+    .catch(error => {
+      console.error(error);
+      return [];
+    });
+}
+
+function formatData (response) {
+  return response.data.map(item => ({
+    type: 'ligblog',
+    date: Date.parse(item.date),
+    title: item.title.rendered,
+    desc: u.extractDescription(item.excerpt.rendered, 60),
+    url: item.link,
+  }));
+}
---
+++ assets/js/services/get-qiita.js
@@ -0,0 +1,29 @@
+import * as u from '../utils';
+import http from '../utils/http';
+const API_PATH = 'https://qiita.com/api/v2/users/mtmtkzm/items';
+
+export default function () {
+  return http.get(API_PATH, {
+    params: {
+      'page': 1,
+      'per_page': 10
+    }
+  })
+    .then(response => {
+      return formatData(response);
+    })
+    .catch(error => {
+      console.error(error);
+      return [];
+    });
+}
+
+function formatData (response) {
+  return response.data.map(item => ({
+    type: 'qiita',
+    date: Date.parse(item.updated_at),
+    title: item.title,
+    desc: u.extractDescription(item.rendered_body),
+    url: item.url,
+  }));
+}
